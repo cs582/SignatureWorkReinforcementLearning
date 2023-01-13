d8ae99b816150b2301ee921de133adee6e2f7af8
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-01-13 14:34:38 -0600

    fixed unpacking. added mask to rewards.

--- ReinforcementLearning/optimizing_dqn.py
+++ ReinforcementLearning/optimizing_dqn.py
@@ -29,8 +29,7 @@ def optimize_dqn(dqn, target, experience_batch, loss_function, gamma, optimizer,
     # Calculate target value
     logger.debug("Calculate target input value")
     target_output = torch.as_tensor(torch.zeros_like(torch.empty(len(experience_batch), y_hat.shape[1], device=device, dtype=torch.double), device=device, dtype=torch.double), dtype=torch.double, device=device)
-    logger.debug(f"Holder shape: {target_output[mask_non_terminal_states].shape} vs next_img shape: {next_state_images.shape}")
-    target_output[mask_non_terminal_states] = torch.add(gamma*target(next_state_images), curr_rewards)
+    target_output[mask_non_terminal_states] = torch.add(gamma*target(next_state_images), curr_rewards[mask_non_terminal_states])
     logger.debug("Target output has been calculated!!!")
 
     # Calculate Loss
