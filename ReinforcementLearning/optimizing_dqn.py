e6e29d506d8adc77a0e0a8c6cfe4170adb6cec64
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-01-17 00:22:18 -0600

    fixed reward at terminal state

--- ReinforcementLearning/optimizing_dqn.py
+++ ReinforcementLearning/optimizing_dqn.py
@@ -29,7 +29,8 @@ def optimize_dqn(dqn, target, experience_batch, loss_function, gamma, optimizer,
     # Calculate target value
     logger.debug("Calculate target input value")
     target_output = torch.as_tensor(torch.zeros_like(torch.empty(len(experience_batch), y_hat.shape[1], device=device, dtype=torch.double), device=device, dtype=torch.double), dtype=torch.double, device=device)
-    target_output[mask_non_terminal_states] = torch.add(gamma*target(next_state_images), curr_rewards[mask_non_terminal_states])
+    target_output[mask_non_terminal_states] = gamma*target(next_state_images)
+    target_output = torch.add(target_output, curr_rewards)
     logger.debug("Target output has been calculated!!!")
 
     # Calculate Loss
