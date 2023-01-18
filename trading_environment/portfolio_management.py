10c62fb93052186c748e6f0aaf4e287d6a3a96ef
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-01-18 11:45:16 -0600

    fixed trading

--- trading_environment/portfolio_management.py
+++ trading_environment/portfolio_management.py
@@ -29,7 +29,7 @@ def portfolio_management(cash, token_portfolio, current_token_prices, current_ga
 
     # Get the names of all tokens in the portfolio
     logger.debug("getting the names of all tokens that need to take action in the action map")
-    tokens = [x for x, y in action_map.items()]
+    tokens = [x for x, y in action_map.items() if y==1.0 or (token_portfolio[x] > 0.0 and y==0.0)]
     logger.debug(f"tokens to be traded: {tokens}")
 
     # Calculate cash per token
