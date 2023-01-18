c8386d1435740afa381783dbc4450975d49b8498
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-01-17 18:42:54 -0600

    Shrinked DNN

--- ReinforcementLearning/q_models.py
+++ ReinforcementLearning/q_models.py
@@ -12,20 +12,18 @@ class Block(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, padding=padding, stride=stride, bias=bias)
         self.bn1 = nn.BatchNorm2d(out_channels)
 
-        self.relu1 = nn.ReLU(inplace=inplace)
+        self.relu = nn.ReLU(inplace=inplace)
 
         self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, padding=padding, stride=stride, bias=bias)
         self.bn2 = nn.BatchNorm2d(out_channels)
 
-        self.relu2 = nn.ReLU(inplace=inplace)
-
     def forward(self, x):
         out = self.conv1(x)
         out = self.bn1(out)
-        out = self.relu1(out)
+        out = self.relu(out)
         out = self.conv2(out)
         out = self.bn2(out)
-        out = self.relu2(out)
+        out = self.relu(out)
         return out
 
 
@@ -37,35 +35,24 @@ class DQN(nn.Module):
         # ResNet-20 backbone
         self.n_classes = n_classes
 
-        # 2 Blocks of 16 channels
+        # 16 channels Block
         self.block1 = Block(in_channels=1, out_channels=16, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
-        self.block2 = Block(in_channels=16, out_channels=16, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
+        self.block2 = Block(in_channels=16, out_channels=32, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
+        self.block3 = Block(in_channels=32, out_channels=64, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
+        self.block4 = Block(in_channels=64, out_channels=128, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
 
-        # 2 Blocks of 32 channels
-        self.block3 = Block(in_channels=16, out_channels=32, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
-        self.block4 = Block(in_channels=32, out_channels=32, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
-
-        # 2 Blocks of 64 channels
-        self.block5 = Block(in_channels=32, out_channels=64, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
-        self.block6 = Block(in_channels=64, out_channels=64, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
-
-        self.fc1 = nn.Linear(n_classes * n_classes * 64, 256)
+        self.fc1 = nn.Linear(n_classes * n_classes * 128, 512)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, 64)
-        self.fc4 = nn.Linear(64, 128)
-        self.fc5 = nn.Linear(128, self.n_classes)
+        self.fc4 = nn.Linear(64, self.n_classes)
 
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x):
-        raw_x = x
         x = self.block1(x)
         x = self.block2(x)
         x = self.block3(x)
         x = self.block4(x)
-        x = self.block5(x)
-        x = self.block6(x)
-        x = torch.add(x, raw_x)
 
         x = torch.flatten(x, 1)
 
@@ -73,7 +60,6 @@ class DQN(nn.Module):
         x = self.fc2(x)
         x = self.fc3(x)
         x = self.fc4(x)
-        x = self.fc5(x)
 
         x = self.softmax(x)
 
@@ -88,52 +74,38 @@ class DuelingDQN(nn.Module):
         # ResNet-20 backbone
         self.n_classes = n_classes
 
-        # 2 Blocks of 16 channels
+        # 16 channels Block
         self.block1 = Block(in_channels=1, out_channels=16, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
-        self.block2 = Block(in_channels=16, out_channels=16, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
+        self.block2 = Block(in_channels=16, out_channels=32, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
+        self.block3 = Block(in_channels=32, out_channels=64, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
+        self.block4 = Block(in_channels=64, out_channels=128, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
 
-        # 2 Blocks of 32 channels
-        self.block3 = Block(in_channels=16, out_channels=32, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
-        self.block4 = Block(in_channels=32, out_channels=32, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
-
-        # 2 Blocks of 64 channels
-        self.block5 = Block(in_channels=32, out_channels=64, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
-        self.block6 = Block(in_channels=64, out_channels=64, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
-
-        self.val1 = nn.Linear(n_classes * n_classes * 64, 256)
+        self.val1 = nn.Linear(n_classes * n_classes * 128, 512)
         self.val2 = nn.Linear(256, 128)
         self.val3 = nn.Linear(128, 64)
-        self.val4 = nn.Linear(64, 128)
-        self.val5 = nn.Linear(128, 1)
+        self.val4 = nn.Linear(64, 1)
 
-        self.adv1 = nn.Linear(n_classes * n_classes * 64, 256)
+        self.adv1 = nn.Linear(n_classes * n_classes * 128, 512)
         self.adv2 = nn.Linear(256, 128)
         self.adv3 = nn.Linear(128, 64)
-        self.adv4 = nn.Linear(64, 128)
-        self.adv5 = nn.Linear(128, self.n_classes)
+        self.adv4 = nn.Linear(64, self.n_classes)
 
     def forward(self, x):
-        raw_x = x
         x = self.block1(x)
         x = self.block2(x)
         x = self.block3(x)
         x = self.block4(x)
-        x = self.block5(x)
-        x = self.block6(x)
-        x = torch.add(x, raw_x)
 
         x = torch.flatten(x, 1)
 
         x_val = self.val1(x)
         x_val = self.val2(x_val)
         x_val = self.val3(x_val)
-        x_val = self.val4(x_val)
-        x_val = self.val5(x_val).repeat(1, self.n_classes)
+        x_val = self.val4(x_val).repeat(1, self.n_classes)
 
         x_adv = self.adv1(x)
         x_adv = self.adv2(x_adv)
         x_adv = self.adv3(x_adv)
         x_adv = self.adv4(x_adv)
-        x_adv = self.adv5(x_adv)
 
         return x_val.add(x_adv - x_adv.sum()/x_adv.shape[0])
