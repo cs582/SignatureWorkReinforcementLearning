85e9bcd67401b051e2715467ddaa044fdf998115
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-01-17 21:03:27 -0600

    added ReLU activation in all models fc1 as well as Xavier init

--- ReinforcementLearning/q_models.py
+++ ReinforcementLearning/q_models.py
@@ -36,13 +36,22 @@ class DQN(nn.Module):
         self.n_classes = n_classes
 
         self.block1 = Block(in_channels=1, out_channels=8, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
+        nn.init.xavier_uniform(self.block1.weight)
         self.block2 = Block(in_channels=8, out_channels=16, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
+        nn.init.xavier_uniform(self.block2.weight)
         self.block3 = Block(in_channels=16, out_channels=32, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
+        nn.init.xavier_uniform(self.block3.weight)
 
         self.fc1 = nn.Linear(n_classes * n_classes * 32, 128)
+        nn.init.xavier_uniform(self.fc1)
         self.fc2 = nn.Linear(128, 128)
+        nn.init.xavier_uniform(self.fc2)
         self.fc3 = nn.Linear(128, 64)
+        nn.init.xavier_uniform(self.fc3)
         self.fc4 = nn.Linear(64, self.n_classes)
+        nn.init.xavier_uniform(self.fc4)
+
+        self.relu = nn.ReLU(inplace=True)
 
         self.softmax = nn.Softmax(dim=-1)
 
@@ -53,7 +62,7 @@ class DQN(nn.Module):
 
         x = torch.flatten(x, 1)
 
-        x = self.fc1(x)
+        x = self.relu(self.fc1(x))
         x = self.fc2(x)
         x = self.fc3(x)
         x = self.fc4(x)
@@ -71,20 +80,32 @@ class DuelingDQN(nn.Module):
         # ResNet-20 backbone
         self.n_classes = n_classes
 
-        # 16 channels Block
         self.block1 = Block(in_channels=1, out_channels=8, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
+        nn.init.xavier_uniform(self.block1.weight)
         self.block2 = Block(in_channels=8, out_channels=16, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
+        nn.init.xavier_uniform(self.block2.weight)
         self.block3 = Block(in_channels=16, out_channels=32, kernel_size=(kernel, kernel), inplace=inplace, bias=bias)
+        nn.init.xavier_uniform(self.block3.weight)
 
         self.val1 = nn.Linear(n_classes * n_classes * 32, 128)
+        nn.init.xavier_uniform(self.val1)
         self.val2 = nn.Linear(128, 128)
+        nn.init.xavier_uniform(self.val2)
         self.val3 = nn.Linear(128, 64)
+        nn.init.xavier_uniform(self.val3)
         self.val4 = nn.Linear(64, 1)
+        nn.init.xavier_uniform(self.val4)
 
         self.adv1 = nn.Linear(n_classes * n_classes * 32, 256)
+        nn.init.xavier_uniform(self.adv1)
         self.adv2 = nn.Linear(256, 128)
+        nn.init.xavier_uniform(self.adv2)
         self.adv3 = nn.Linear(128, 64)
+        nn.init.xavier_uniform(self.adv3)
         self.adv4 = nn.Linear(64, self.n_classes)
+        nn.init.xavier_uniform(self.adv4)
+
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         x = self.block1(x)
@@ -93,12 +114,12 @@ class DuelingDQN(nn.Module):
 
         x = torch.flatten(x, 1)
 
-        x_val = self.val1(x)
+        x_val = self.relu(self.val1(x))
         x_val = self.val2(x_val)
         x_val = self.val3(x_val)
         x_val = self.val4(x_val).repeat(1, self.n_classes)
 
-        x_adv = self.adv1(x)
+        x_adv = self.relu(self.adv1(x))
         x_adv = self.adv2(x_adv)
         x_adv = self.adv3(x_adv)
         x_adv = self.adv4(x_adv)
