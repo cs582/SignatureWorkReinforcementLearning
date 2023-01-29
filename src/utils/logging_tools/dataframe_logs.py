11853129df594941efa9fa1e194b85de3f8fcb21
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-01-29 22:23:55 +0800

    fixed logs

--- src/utils/logging_tools/dataframe_logs.py
+++ src/utils/logging_tools/dataframe_logs.py
@@ -8,16 +8,16 @@ def prices_and_gas_preview(logger, token_prices, gas_prices):
 
     logger.debug(f"""
     Head of the gas prices:
-    {gas_prices.head(5)}
+    {gas_prices[:5]}
     Tail of the gas prices:
-    {gas_prices.tail(5)}
+    {gas_prices[:-5]}
     """)
 
 
 def images_preview(logger, images):
     logger.debug(f"""
     Head of the images:
-    {images.head(5)}
+    {images[:5]}
     Tail of the images:
-    {images.tail(5)}
+    {images[-5:]}
     """)
