a84826c070409cdde7407ddb14236fc6d1cd0dff
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-01-31 09:53:47 +0800

    logs to check sharpe

--- src/trading_environment/environment.py
+++ src/trading_environment/environment.py
@@ -228,7 +228,9 @@ class Environment:
 
         # Calculate sharpe ratio
         stddev = np.std(self.daily_roi_history)
-        sharpe = (n_days ** 0.5) * np.mean(self.daily_roi_history) / (stddev if stddev != np.nan else 1.0)
+        mmean = np.mean(self.daily_roi_history)
+        logger.debug(f"std = {stddev} and mean = {mmean}")
+        sharpe = (n_days ** 0.5) * mmean / (stddev if stddev != np.nan else 1.0)
         self.sharpe_history.append(float(sharpe))
         logger.info(f"Sharpe Ratio: {sharpe}")
 
