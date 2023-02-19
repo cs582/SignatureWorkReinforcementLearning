8c1392aa0dce931b515e5bc63c49f03343a1bb2d
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-02-19 21:11:41 +0800

    changed collors

--- src/environment/trading_environment/token_management.py
+++ src/environment/trading_environment/token_management.py
@@ -64,8 +64,13 @@ def buy_position(day, curr_action, cash, base_gas, gas_limit, priority_fee, toke
 
 
 def swap_position(day, prev_action, curr_action, cash, base_gas, gas_limit, priority_fee, tokens_to_sell, tokens_to_buy, portfolio, token_prices):
+    # Selling current tokens to collect money
     cash_earned, tokens_value, net_worth, portfolio = sell_position(day=day, curr_action=curr_action, tokens=tokens_to_sell, base_gas=base_gas, gas_limit=gas_limit, priority_fee=priority_fee, portfolio=portfolio, token_prices=token_prices)
     total_cash_to_buy = cash_earned + cash
+
+    # Buying tokens
     total_cash, tokens_value, net_worth, portfolio = buy_position(day=day, curr_action=curr_action, cash=total_cash_to_buy, tokens=tokens_to_buy, base_gas=base_gas, gas_limit=gas_limit, priority_fee=priority_fee, portfolio=portfolio, token_prices=token_prices)
+
+    # Show swap changes
     show_swap_position_orange(day=day, prev_action=prev_action, curr_action=curr_action, tokens_value=tokens_value, cash=cash, net_worth=net_worth)
     return total_cash, tokens_value, net_worth, portfolio
