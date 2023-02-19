a83bac61e7096ef80066ff711d7682a7cad2445e
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-02-19 20:02:50 +0800

    Added prev and curr action to UNKNOWN POSITION.

--- src/environment/trading_environment/portfolio_management.py
+++ src/environment/trading_environment/portfolio_management.py
@@ -45,5 +45,5 @@ def portfolio_management(day, prev_action, curr_action, position, cash, token_po
         logger.info("Neutral Position.")
         total_cash_remaining, total_tokens_value_remaining, curr_total_net_worth = neutral_position(day=day, cash=cash, curr_action=curr_action, portfolio=token_portfolio, token_prices=current_token_prices)
     else:
-        logger.warn(f"UNKNOWN POSITION! AT DAY {day}")
+        logger.warn(f"UNKNOWN POSITION! AT DAY {day}. Previous Action: {prev_action} and Curr Action: {curr_action}")
     return token_portfolio, curr_total_net_worth, total_cash_remaining, total_tokens_value_remaining
