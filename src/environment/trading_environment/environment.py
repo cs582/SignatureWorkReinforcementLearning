8cd44fcc2ec2919574fa6d059f7e4568cf1d8b3e
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-02-21 14:16:33 +0800

    changed reward function rule

--- src/environment/trading_environment/environment.py
+++ src/environment/trading_environment/environment.py
@@ -252,8 +252,13 @@ class Environment:
         self.sharpe_history.append(float(sharpe))
         logger_main.info(f"Sharpe Ratio: {sharpe}")
 
-        # Update environment current state
-        reward = self.sharpe_history[-1] if self.reward_metric == "sharpe" else self.daily_roi_history[-1]
+        # Calculate REWARD, this is a short window of rewards
+        wd_size = min(len(self.daily_roi_history), 25)
+        short_window = self.daily_roi_history[:-wd_size]
+        rew_std = np.std(short_window)
+        rew_mean = (n_days ** 0.5) * np.mean(short_window)
+        reward = rew_mean/rew_std if rew_std!=0.0 else rew_mean
+        #reward = self.sharpe_history[-1] if self.reward_metric == "sharpe" else self.daily_roi_history[-1]
 
         # Show the reward on screen in CYAN
         show_rewards(mode=mode, day=trading_day, roi=gross_roi, sharpe=sharpe, reward=reward)
