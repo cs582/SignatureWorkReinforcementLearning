ca08b6fe068f43fa30d4c59da76b924a09659249
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-02-21 21:22:15 +0800

    added print statements

--- src/preprocessing/data_handling/preprocessing.py
+++ src/preprocessing/data_handling/preprocessing.py
@@ -29,6 +29,7 @@ def preprocessing_snapshots(prices, size):
     :param size:        --int, size of the covariance matrices, it will take size number of days historical data to calculate the covariance matrix
     :return: X          --list, returns a list of 2D covariance matrices
     """
+    print("PROCESSING SNAPSHOTS")
     X = []
 
     n = prices[0].shape[0] - size
@@ -36,9 +37,13 @@ def preprocessing_snapshots(prices, size):
         x = []
         for k in range(0, len(prices)):
             prices_view = prices[k].iloc[i:i + size].values
+            print(prices_view)
             price_snapshot = np.nan_to_num(prices_view, nan=0)
+            print(price_snapshot)
             x.append(price_snapshot)
+        print(x)
         X.append(x)
+    print(X[-1])
 
     return X
 
@@ -50,6 +55,7 @@ def prepare_dataset(tokens_to_use, token_prices, use, lookback):
     :param lookback:            --int, when using covariance, it tells how many days back to look at. To trade for n days, it would be necessary to have n+lookback days as input.
     :return: X                  --np.array, returns the list of 2D input matrices
     """
+    print("PREPARE DATASET")
     X_matrices = None
 
     token_prices = token_prices[tokens_to_use].astype(np.float64)
