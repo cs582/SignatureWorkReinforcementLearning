a9a862814b9d111fa93b01a30907afcd76c94bc5
Carlos Gustavo Salas Flores <cs582@duke.edu>
2023-02-25 16:57:05 +0800

    Portfolio Selection and figures folder updated

--- src/environment/figures/figures.py
+++ src/environment/figures/figures.py
@@ -6,27 +6,44 @@ data_file = "data/raw//ClosePriceData_2022-10-01_to_2022-08-21.csv"
 portfolio_file = "portfolios//portfolios.json"
 
 
-env = Environment(
-    trading_days=800,
-    token_prices_address=data_file,
-    gas_address=None,
-    gas_limit=1000,
-    portfolio_json=portfolio_file,
-    portfolio_to_use=1,
-    initial_cash=1000,
-    buy_limit=100,
-    sell_limit=100,
-    priority_fee=10,
-    use_change=True,
-    use_covariance=True,
-    reward_metric='sharpe',
-    device='cuda'
-)
+def covariance_matrices(n_figures=10, save_dir="data/figures/covariance_matrices"):
+    env = Environment(
+        trading_days=800,
+        token_prices_address=data_file,
+        gas_address=None,
+        gas_limit=1000,
+        portfolio_json=portfolio_file,
+        portfolio_to_use=1,
+        initial_cash=1000,
+        priority_fee=10,
+        use=2,
+        reward_metric='sharpe',
+        device='cuda'
+    )
 
-env.preload_prices()
-
-
-def covariance_matrices(n_figures=10, save_dir="data/figures"):
+    env.preload_prices()
     for i in range(0, n_figures):
         plt.imshow(env.database_train[i][0])
-        plt.savefig(f"{save_dir}//figure_{i}.png")
+        plt.savefig(f"{save_dir}//covariance_at_timestep_{i+1}.png")
+
+
+def shapshot_figures(n_figures=10, save_dir="data/figures/history_snapshot"):
+    env = Environment(
+        trading_days=800,
+        token_prices_address=data_file,
+        gas_address=None,
+        gas_limit=1000,
+        portfolio_json=portfolio_file,
+        portfolio_to_use=1,
+        initial_cash=1000,
+        priority_fee=10,
+        lookback=20,
+        use=3,
+        reward_metric='sharpe',
+        device='cuda'
+    )
+
+    env.preload_prices()
+    for i in range(0, n_figures):
+        plt.imshow(env.database_train[i][0])
+        plt.savefig(f"{save_dir}//snapshot_at_timestep_{i+1}.png")
